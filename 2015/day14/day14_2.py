688d9d79c7e40c5f9a3ac22a2173894e7622caad
Kai Berszin <mail@kaibersz.in>
2021-12-13 18:38:15 +0100

    Add solution for 2015 day14

---
+++ 2015/day14/day14_2.py
@@ -0,0 +1,33 @@
+import sys, os
+sys.path.append(os.path.abspath("."))
+import aoc
+
+reindeers = {}
+
+scores = {}
+
+for l in aoc.aoc():
+    [name, _, _, speed, _, _, time, _, _, _, _, _, _, rest, _] = l.split(" ")
+    scores[name] = 0
+    reindeers[name] = {'speed': int(speed), 'flying_time': int(time), 'rest_time': int(rest), 'resting_left': 0, 'flying_left': int(time), 'dist_traveled': 0}
+
+
+for t in range(0, 2503):
+    for reindeer in reindeers.keys():
+        name = reindeer
+        info = reindeers[name]
+        if info['flying_left'] > 0:
+            reindeers[name]['flying_left'] = info['flying_left']-1
+            reindeers[name]['dist_traveled'] = info['dist_traveled'] + info['speed']
+            if info['flying_left'] == 0 and info['resting_left'] == 0:
+                reindeers[name]['resting_left'] = info['rest_time']
+        elif info['resting_left'] > 0:
+            reindeers[name]['resting_left'] = info['resting_left']-1
+            if info['resting_left'] == 0:
+                reindeers[name]['flying_left'] = info['flying_time']
+    best_dist = max(map(lambda x : reindeers[str(x)]['dist_traveled'], reindeers))
+
+    for winning_deer in filter(lambda deer: reindeers[deer]['dist_traveled'] == best_dist, reindeers):
+        scores[winning_deer] = scores[winning_deer] + 1
+
+print("The winning reindeer has ", max(scores.values()), "points")
